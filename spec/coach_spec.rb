f5616a0d8bb248c8eadb70401e5b9ddfc99c1fd9
qualityofarmor <dmcalerney.trash@gmail.com>
2015-12-22 10:38:15 -0800

    Added test to confirm the functionality that deals with problem #2

--- spec/coach_spec.rb
+++ spec/coach_spec.rb
@@ -37,12 +37,13 @@ describe Coach do
     expect(coach.student_count).to eq 3
   end
 
+# PROBLEM #1
   it 'is assigned students in accordance with a collective and even distribution' do
       coach_list = []
       coach_list << Coach.create!(name: 'Coach Halford')
       coach_list << Coach.create!(name: 'Coach Pollard')
       coach_list << Coach.create!(name: 'Coach Danzig')
-      
+
       student_list = []
       student_list << Student.create({name: 'Ava Minor'})
       student_list << Student.create({name: 'Bee Sharpe'})
@@ -65,5 +66,50 @@ describe Coach do
 
   end
 
+# PROBLEM 2
+it 'is assigned students evenly despite the existance of previously assigned students' do
+    coach_list = []
+    coach_list << Coach.create!(name: 'Coach Halford')
+    coach_list << Coach.create!(name: 'Coach Pollard')
+    coach_list << Coach.create!(name: 'Coach Danzig')
+
+    student_list = []
+    student_list << Student.create({name: 'Thomas Waits'})
+    student_list << Student.create({name: 'Brian Wilson'})
+    student_list << Student.create({name: 'David Byrne'})
+    student_list << Student.create({name: 'Tobin Sprout'})
+    student_list << Student.create({name: 'Josaphene Wiggs'})
+    student_list << Student.create({name: 'Herbie Flowers'})
+    student_list << Student.create({name: 'Keith Richards'})
+
+    assign_students(student_list, coach_list)
+    # coach_list.sort_by! { |c| c.student_count }
+    puts "\n --- PROBLEM Num. 2 POST ASSIGNMENT NUMBERS -- "
+    coach_list.each do |c|
+      puts "#{c[:name]} -> #{c.student_count}"
+    end
+
+    expect(coach_list.first.student_count).to eq 3
+    expect(coach_list.second.student_count).to eq 2
+    expect(coach_list.third.student_count).to eq 2
+
+    student_list = []
+    student_list << Student.create({name: 'Chris Bell'})
+    student_list << Student.create({name: 'Alex Chilton'})
+    student_list << Student.create({name: 'Joan Jett'})
+    student_list << Student.create({name: 'Kathleen Hanna'})
+    student_list << Student.create({name: 'Elliott Smith'})
+
+    assign_students(student_list, coach_list)
+    puts "\n --- PROBLEM Num. 2 POST POST ASSIGNMENT NUMBERS -- "
+    coach_list.each do |c|
+      puts "#{c[:name]} -> #{c.student_count}"
+    end
+
+    expect(coach_list.first.student_count).to eq 4
+    expect(coach_list.second.student_count).to eq 4
+    expect(coach_list.third.student_count).to eq 4
+end
+# PROBLEM 3
 
 end
